9085dde8c76ea1ee83c2598f000d9098b6611b28
Hwiyeon Cho <hwyn.cho@outlook.com>
2019-02-16 01:52:18 +0900

    :pencil: Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const bodyParser = require("body-parser");
-const express = require('express');
+const express = require("express");
+
+const port = 8080;
 
 const app = express();
-const appInfo = {
-  "host": "127.0.0.1",
-  "port": 8080
-}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,4 +13,4 @@ app.get("/api/read/:apiName", require(__dirname + "/routes/read"));
 app.put("/api/update/:apiName", require(__dirname + "/routes/update"));
 app.delete("/api/delete/:apiName", require(__dirname + "/routes/delete"));
 
-app.listen(appInfo.port, () => { console.log("Server start."); });
+app.listen(port, () => { console.log("Server start."); });
